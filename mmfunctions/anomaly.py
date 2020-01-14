a22c19bd03375f95d0fef8d5068ea02f141c40ac
Markus Müller <markus_mueller@de.ibm.com>
2020-01-14 22:09:53 +0100

    fix

--- mmfunctions/anomaly.py
+++ mmfunctions/anomaly.py
@@ -17,7 +17,7 @@ import numpy as np
 import scipy as sp
 
 #  for Spectral Analysis
-from scipy import signal,fft
+from scipy import signal,fftpack
 # from scipy.stats import energy_distance
 # from sklearn import metrics
 from sklearn.covariance import EllipticEnvelope, MinCovDet
@@ -736,7 +736,7 @@ class FFTbasedGeneralizedAnomalyScore2(GeneralizedAnomalyScore2):
         )
         slices = []
         for slice in slices_:
-            slices.append(sp.fft.rfft(slice))
+            slices.append(fftpack.rfft(slice))
 
         return slices
 
