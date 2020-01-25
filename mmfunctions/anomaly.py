3ef64b7fcc97f722194cdf051ca53233bd4bba04
Markus Müller <markus_mueller@de.ibm.com>
2020-01-25 10:07:00 +0100

    update

--- mmfunctions/anomaly.py
+++ mmfunctions/anomaly.py
@@ -108,33 +108,6 @@ def series_filter(values, kernel_size=3):
     return filter_values
 
 
-def extrapolate_next(values):
-    """
-    Extrapolates the next value by sum up the slope of the last value with previous values.
-    :param values: a list or numpy array of time-series
-    :return: the next value of time-series
-    """
-
-    last_value = values[-1]
-    slope = [(last_value - v) / i for (i, v) in enumerate(values[::-1])]
-    slope[0] = 0
-    next_values = last_value + np.cumsum(slope)
-
-    return next_values
-
-
-def marge_series(values, extend_num=5, forward=5):
-
-    next_value = extrapolate_next(values)[forward]
-    extension = [next_value] * extend_num
-
-    if isinstance(values, list):
-        marge_values = values + extension
-    else:
-        marge_values = np.append(values, extension)
-    return marge_values
-
-
 # Saliency class
 #  see https://www.inf.uni-hamburg.de/en/inst/ab/cv/research/research1-visual-attention.html
 class Saliency(object):
