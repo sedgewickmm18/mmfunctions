ccc172c2bb169a721144ccf01572a2758775478c
Markus Müller <markus_mueller@de.ibm.com>
2020-01-31 16:44:14 +0100

    update

--- mmfunctions/anomaly.py
+++ mmfunctions/anomaly.py
@@ -906,10 +906,11 @@ class AlertExpressionWithFilter(BaseEvent):
         logger.info('AlertExpressionWithFilter  regexp: ' + expr)
 
         try:
-            df[self.alert_name] = np.logical_and(
+            np_res = np.logical_and(
                 np.where(df[self.dimension_name] == self.dimension_value, True, False),
                 np.where(eval(expr), True, False))
-            logger.info('AlertExpressionWithFilter  final result: ' + str(df[self.alert_name]))
+            logger.info('AlertExpressionWithFilter  final result: ' + str(np_res))
+            df[self.alert_name] = np_res
 
         except Exception as e:
             logger.info('AlertExpressionWithFilter  eval failed with ' + str(e))
