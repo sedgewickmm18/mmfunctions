aaf36e16e76f74e811219e7bdffc9396820b9b31
Markus Müller <markus_mueller@de.ibm.com>
2020-04-14 10:48:24 +0200

    update

--- mmfunctions/anomaly.py
+++ mmfunctions/anomaly.py
@@ -1464,3 +1464,50 @@ class SimpleAnomaly(BaseRegressor):
             UIFunctionOutMulti(name='alerts', datatype=bool, cardinality_from='targets', is_datatype_derived=False, ))
 
         return (inputs, outputs)
+
+
+from iotfunctions.base import SimpleAggregator
+
+def _no_datatype_aggregator_output():
+    return {'name': 'name',
+            'description': 'Enter a name for the data item that is produced as a result of this calculation.'}.copy()
+
+def _general_aggregator_input():
+    return {'name': 'source', 'description': 'Select the data item that you want to use as input for your calculation.',
+            'type': 'DATA_ITEM', 'required': True, }.copy()
+
+def _general_aggregator_output():
+    output_item = _no_datatype_aggregator_output()
+    output_item['dataTypeFrom'] = 'source'
+    return output_item
+
+def _number_aggregator_output():
+    output_item = _no_datatype_aggregator_output()
+    output_item['dataType'] = 'NUMBER'
+    return output_item
+
+def _generate_metadata(cls, metadata):
+    common_metadata = {'name': cls.__name__, 'moduleAndTargetName': '%s.%s' % (cls.__module__, cls.__name__),
+                       'category': 'AGGREGATOR', 'input': [_general_aggregator_input()], 'output': [_general_aggregator_output()]}
+    common_metadata.update(metadata)
+    return common_metadata
+
+class AggregateWithCalculation(SimpleAggregator):
+
+    @classmethod
+    def metadata(cls):
+        return _generate_metadata(cls, {'description': 'Create aggregation using expression on a data item.',
+            'input': [_general_aggregator_input(), {'name': 'expression',
+                'description': 'The expression. Use ${GROUP} to reference the current grain. \
+                 All Pandas Series methods can be used on the grain. For example, ${GROUP}.max() - ${GROUP}.min().',
+                'type': 'CONSTANT', 'required': True, 'dataType': 'LITERAL'}],
+            'output': [_no_datatype_aggregator_output()], 'tags': ['EVENT', 'JUPYTER']})
+
+    def __init__(self, expression=None):
+        if expression is None or not isinstance(expression, str):
+            raise RuntimeError("argument expression must be provided and must be a string")
+
+        self.expression = expression
+
+    def execute(self, group):
+        return eval(re.sub(r"\$\{GROUP\}", r"group", self.expression))
