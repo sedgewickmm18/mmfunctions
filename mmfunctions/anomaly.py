68ab58fed3c4c64db15157320f9258c9fcdba5fb
Markus Müller <markus_mueller@de.ibm.com>
2020-04-21 20:15:38 +0200

    update SimpleRegressor

--- mmfunctions/anomaly.py
+++ mmfunctions/anomaly.py
@@ -20,6 +20,7 @@ import scipy as sp
 #  for Spectral Analysis
 from scipy import signal, fftpack
 # from scipy.stats import energy_distance
+from sklearn.utils import check_X_y
 from sklearn import metrics
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
@@ -41,7 +42,7 @@ import logging
 # import warnings
 # import json
 # from sqlalchemy import Column, Integer, String, Float, DateTime, Boolean, func
-from iotfunctions.base import (BaseTransformer, BaseSimpleAggregator, BaseRegressor, BaseEvent, BaseEstimatorFunction)
+from iotfunctions.base import (BaseTransformer, BaseRegressor, BaseEvent, BaseEstimatorFunction)
 from iotfunctions.bif import (AlertHighValue)
 from iotfunctions.ui import (UISingle, UIMultiItem, UIFunctionOutSingle, UISingleItem, UIFunctionOutMulti,
                              UIExpression)
@@ -1384,16 +1385,18 @@ class SimpleRegressor(BaseEstimatorFunction):
             df_copy[m] = None
 
         for entity in entities:
-            # try:
+            try:
+                check_X_y(df_copy.loc[[entity]][self.features].values)
                 dfe = super()._execute(df_copy.loc[[entity]], entity)
                 print(df_copy.columns)
                 # for c in self.predictions:
                 df_copy.loc[entity, self.predictions] = dfe[self.predictions]
                 # df_copy = df_copy.loc[[entity]] = dfe
                 print(df_copy.columns)
-            # except Exception as e:
-                # logger.info('GBMRegressor for entity ' + str(entity) + ' failed with: ' + str(e))
-                # continue
+            except Exception as e:
+                logger.info('GBMRegressor for entity ' + str(entity) + ' failed with: ' + str(e))
+                df_copy.loc[entity, self.predictions] = 0
+                pass
         return df_copy
 
     @classmethod
@@ -1465,64 +1468,3 @@ class SimpleAnomaly(BaseRegressor):
             UIFunctionOutMulti(name='alerts', datatype=bool, cardinality_from='targets', is_datatype_derived=False, ))
 
         return (inputs, outputs)
-
-
-def _no_datatype_aggregator_output():
-    return {'name': 'name',
-            'description': 'Enter a name for the data item that is produced as a result of this calculation.'}.copy()
-
-
-def _general_aggregator_input():
-    return {'name': 'source', 'description': 'Select the data item that you want to use as input for your calculation.',
-            'type': 'DATA_ITEM', 'required': True, }.copy()
-
-
-def _general_aggregator_output():
-    output_item = _no_datatype_aggregator_output()
-    output_item['dataTypeFrom'] = 'source'
-    return output_item
-
-
-def _number_aggregator_output():
-    output_item = _no_datatype_aggregator_output()
-    output_item['dataType'] = 'NUMBER'
-    return output_item
-
-
-def _generate_metadata(cls, metadata):
-    common_metadata = {'name': cls.__name__, 'moduleAndTargetName': '%s.%s' % (cls.__module__, cls.__name__),
-                       'category': 'AGGREGATOR', 'input': [_general_aggregator_input()], 'output': [_general_aggregator_output()]}
-    common_metadata.update(metadata)
-    return common_metadata
-
-
-class SimpleAggregator(BaseSimpleAggregator):
-
-    @classmethod
-    def metadata(cls):
-        return _generate_metadata(cls, {'description': 'Create aggregation using expression on a data item.',
-                                        'input': [_general_aggregator_input(), {'name': 'expression',
-                                        'description': 'The expression. Use ${GROUP} to reference the current grain. \
-                                        All Pandas Series methods can be used on the grain. For example, ${GROUP}.max() - ${GROUP}.min().',
-                                        'type': 'CONSTANT', 'required': True, 'dataType': 'LITERAL'}],
-                                        'output': [_no_datatype_aggregator_output()], 'tags': ['EVENT', 'JUPYTER']})
-
-    def __init__(self, source=None, expression=None):
-        if expression is None or not isinstance(expression, str):
-            raise RuntimeError("argument expression must be provided and must be a string")
-
-        self.input_items = source
-        self.expression = expression
-
-    def execute(self, group):
-        return eval(re.sub(r"\$\{GROUP\}", r"group", self.expression))
-
-    @classmethod
-    def build_ui(cls):
-        inputs = []
-        inputs.append(UIMultiItem(name='source', datatype=None, description=('Choose the data items'
-                                                                                  ' that you would like to'
-                                                                                  ' aggregate'),
-                                  output_item='name', is_output_datatype_derived=True))
-        inputs.append(UIExpression(name='expression', description='Paste in or type an AS expression'))
-        return (inputs, [])
