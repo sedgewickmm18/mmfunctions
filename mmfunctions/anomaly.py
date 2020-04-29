e4c0581ad770fa06eba8ad4ee65151072cd07aa9
Markus Müller <markus_mueller@de.ibm.com>
2020-04-29 15:47:49 +0200

    update

--- mmfunctions/anomaly.py
+++ mmfunctions/anomaly.py
@@ -759,6 +759,8 @@ class GeneralizedAnomalyScore(BaseTransformer):
                 diff = temperature.size - pred_score.size
                 timesTS = np.linspace(diff // 2 + diff % 2, temperature.size - diff//2 + 1, diff)
 
+                print(self.whoami + '   Entity: ' + str(entity) + ', result shape: ' + str(timesTS.shape) +
+                      ' score shape: ' + str(pred_score.shape))
                 logger.debug(self.whoami + '   Entity: ' + str(entity) + ', result shape: ' + str(timesTS.shape) +
                              ' score shape: ' + str(pred_score.shape))
 
