0bab4a4bed1bc1d0312265b679efd7fa6dce8b61
Markus Müller <markus_mueller@de.ibm.com>
2021-01-27 18:34:40 +0100

    update

--- mmfunctions/customer.py
+++ mmfunctions/customer.py
@@ -123,6 +123,7 @@ class UnrollData(BaseTransformer):
         for ix, row in df.iterrows():
             # columns with 15 elements
             #device_id = ix[0].replace('Device','Shadow') - device id is identical !
+            print('Row is ', row)
             device_id = ix[0]
 
             None5 = [None, None, None, None, None]
